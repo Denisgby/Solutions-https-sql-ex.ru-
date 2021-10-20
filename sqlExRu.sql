1738b155bef24ce4f299526065d4cb662502df4d
Denisgby <denis.gby@gmail.com>
2021-10-20 23:35:00 +0300

    t6

--- sqlExRu.sql
+++ sqlExRu.sql
@@ -50,3 +50,9 @@ GO
 
 --Задание: 6
 --Для каждого производителя, выпускающего ПК-блокноты c объёмом жесткого диска не менее 10 Гбайт, найти скорости таких ПК-блокнотов. Вывод: производитель, скорость.
+
+SELECT DISTINCT Product.maker, Laptop.speed
+FROM Product JOIN Laptop ON product.model=laptop.model 
+WHERE Laptop.hd >= 10
+GO
+----------------------
